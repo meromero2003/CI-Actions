d17d0099030974902437255b18e0a97872fd4e71
Angel MR <angeldanielmero@hotmail.com>
2025-06-12 16:13:19 -0600

    wip

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
-const app = express();
+app = express();
+
+const PORT = 3000; // Cambia si usas otro puerto
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  import('open').then(open => open.default(`http://localhost:${PORT}`)); // Importa y ejecuta open
 });
 
 module.exports = app;
